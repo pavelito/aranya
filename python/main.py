dea6b6f701132194f3f34bc17a5948a6547a095b
Pavel Pratyush <akpratyush@gmail.com>
2020-07-14 01:13:02 +0200

    Dockerize and Normalize

--- python/main.py
+++ python/main.py
@@ -3,11 +3,11 @@ import asyncio
 import json, time
 
 
-async def countdown(number):
-    counter = number
-    while counter > 0:
-        counter = counter - 1
-    print (f"Countdown done for {number}")
+async def is_prime(number):
+    for i in range(2, int(number/2)):
+        if number%i == 0:
+            return False
+    return True
 
 async def fetch(session, url):
     async with session.get(url) as response:
@@ -21,21 +21,28 @@ async def get_new_stories():
 async def get_story_detail(story_id):
     async with aiohttp.ClientSession() as session:
         html = await fetch(session, f"https://hacker-news.firebaseio.com/v0/item/{story_id}.json?print=pretty")
-        return json.loads(html)
+        story_detail = json.loads(html)
+        prime_result = await is_prime(story_detail['time'])
+        print (f"Story has title {story_detail['title']} and ID is Prime - {prime_result}")
+
+        
 
 async def main():
     new_stories = await get_new_stories()
-    for story_id in new_stories:
-        story_detail = await get_story_detail(story_id)
-        print (f"Story has title {story_detail['title']}")
-        await countdown(story_detail['time'])
-
-    
+    worker_count = 25
+    limit = len(new_stories)
+    for i in range(limit):
+        tasks = []
+        for j in range(i, i+worker_count):
+            if j == limit:
+                break
+            tasks.append(asyncio.create_task(get_story_detail(new_stories[j])))
+        await asyncio.gather(*tasks)
+        i = i+worker_count    
 
 if __name__ == '__main__':
     start_time = time.time()
-    #print(start_time)
-    loop = asyncio.get_event_loop()
-    loop.run_until_complete(main())
+    print(start_time)
+    asyncio.run(main())
     elapsed = time.time() - start_time
     print(f"Total seconds to finish - {elapsed}")
